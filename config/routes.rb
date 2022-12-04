147c794ff65fe9ce9628c64af769fd6a3b9593f9
annakynutsya <kunits.anya@gmail.com>
2022-12-04 16:50:50 +0200

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root to: 'questions#index'
+  resources :questions
+  
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
+end
